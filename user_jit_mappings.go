83cce6bb04f9b80c8823b02a2f6961bb6499377e
Gregory Stotts <mr.stotts@gmail.com>
2023-06-25 02:42:00 -0500

    Update

--- user_jit_mappings.go
+++ user_jit_mappings.go
@@ -13,21 +13,19 @@ func (c *Client) ListGroupMappings(auth_server_id int) ([]GroupMapping, error) {
 	return resp, err
 }
 
-func (c *Client) AddGroupMapping(auth_server_id int, mapping GroupMapping) error {
+func (c *Client) AddGroupMapping(auth_server_id int, mapping []GroupMapping) error {
 	// Adds a new group mapping to the mapping list
 
 	return c.makeRequest(http.MethodPost, fmt.Sprintf("/v2/prototype/authenticationserver/%d/insert_group_mapping", auth_server_id), mapping, nil)
 }
 
-func (c *Client) UpdateAllGroupMappings(auth_server_id int, mapping []GroupMapping) ([]GroupMapping, error) {
+func (c *Client) UpdateAllGroupMappings(auth_server_id int, mapping []GroupMapping) error {
 	// Overwrites the group mapping with the given mapping list
 
-	var resp []GroupMapping
-	err := c.makeRequest(http.MethodGet, fmt.Sprintf("/v2/prototype/authenticationserver/%d/update_group_mapping", auth_server_id), mapping, &resp)
-	return resp, err
+	return c.makeRequest(http.MethodGet, fmt.Sprintf("/v2/prototype/authenticationserver/%d/update_group_mapping", auth_server_id), mapping, nil)
 }
 
-func (c *Client) DeleteGroupMapping(auth_server_id int, mapping GroupMapping) error {
+func (c *Client) DeleteGroupMapping(auth_server_id int, mapping []GroupMapping) error {
 	// Deletes an existing group mapping.  Non-existent mappings are ignored.
 
 	return c.makeRequest(http.MethodDelete, fmt.Sprintf("/v2/prototype/authenticationserver/%d/delete_group_mapping", auth_server_id), mapping, nil)
