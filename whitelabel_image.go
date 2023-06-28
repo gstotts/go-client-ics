c6c2d3ad2dda75ffec3e17cc75011c3ab43e884e
Gregory Stotts <mr.stotts@gmail.com>
2023-06-27 21:48:25 -0500

    Whitelabel

---
+++ whitelabel_image.go
@@ -0,0 +1,9 @@
+package insightcloudsecClient
+
+import "net/http"
+
+func (c *Client) SetCustomLogo(image_url string) error {
+	// Sets the logo to a custom image png.  Can be base64-encoded png url.  Approx 450 x 115 px
+
+	return c.makeRequest(http.MethodPost, "/v2/public/whiltelabel/set-custom-logo", WhitelabelImage{image_url}, nil)
+}
