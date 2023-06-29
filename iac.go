4373f8e674fb2bc29997a816ef6bccfa81c8a59d
Gregory Stotts <mr.stotts@gmail.com>
2023-06-29 00:42:23 -0500

    Initial work IAC

--- iac.go
+++ iac.go
@@ -1,11 +1,40 @@
 package insightcloudsecClient
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (c *Client) ListIACConfigs() ([]IACConfig, error) {
 	// Lists IAC Configurations and basic stats
 
 	var resp []IACConfig
 	err := c.makeRequest(http.MethodGet, "/v3/iac/configs", nil, &resp)
+	return resp, err
+}
+
+func (c *Client) InitiateIACScan(details Scan, simplified bool) (ScanResults, error) {
+	// Initates an IAC Scan and returns results as detailed or simplified
+
+	url := "/v3/iac/scan"
+	if simplified {
+		url = fmt.Sprintf("%s?readable=true", url)
+	}
+
+	var resp ScanResults
+	err := c.makeRequest(http.MethodPost, url, details, &resp)
+	return resp, err
+}
+
+func (c *Client) GetIACScan(build_id int, simplified bool) (ScanResults, error) {
+	// Returns the scan results for given build_id
+
+	url := fmt.Sprintf("/v3/iac/scans/%d", build_id)
+	if simplified {
+		url = fmt.Sprintf("%s?readable=true", url)
+	}
+
+	var resp ScanResults
+	err := c.makeRequest(http.MethodGet, url, nil, &resp)
 	return resp, err
 }
--- models_iac.go
+++ models_iac.go
@@ -22,3 +22,69 @@ type IACConfig struct {
 	TFCCount               int      `json:"tfc_count"`
 	LocalExceptionsEnabled bool     `json:"local_exceptions_enabled"`
 }
+
+type Scan struct {
+	Name     string `json:"scan_name"`
+	Template string `json:"scan_template"`
+	Config   string `json:"config_name"`
+	Author   string `json:"author_name"`
+	Provider string `json:"iac_provider"`
+}
+
+type ScanResults struct {
+	BuildID         int                    `json:"build_id"`
+	Config          string                 `json:"config_name"`
+	Details         ScanDetails            `json:"details,omitempty"`
+	Errors          []string               `json:"errors"`
+	Message         string                 `json:"message"`
+	Resources       []ScanResource         `json:"resources,omitempty"`
+	ResourceMapping map[string]interface{} `json:"resource_mapping,omitempty"`
+	ResultsURL      string                 `json:"scan_results"`
+	Stacktrace      []string               `json:"stacktrace"`
+	Status          string                 `json:"status"`
+	Success         bool                   `json:"success"`
+}
+
+type ScanDetails struct {
+	FailedInsights   []InsightScanResults `json:"failed_insights"`
+	FailedResources  int                  `json:"failed_resources"`
+	PassedInsights   []InsightScanResults `json:"passed_insights"`
+	PassedResources  int                  `json:"passed_resources"`
+	SkippedInsights  []InsightScanResults `json:"skipped_insights"`
+	SkippedResources int                  `json:"skipped_resources"`
+	TotalInsights    int                  `json:"total_insights"`
+	TotalResources   int                  `json:"total_resources"`
+	WarnedInsights   []InsightScanResults `json:"warned_insights"`
+	WarnedResources  int                  `json:"warned_resources"`
+}
+
+type InsightScanResults struct {
+	ID            int      `json:"id"`
+	Name          string   `json:"name"`
+	Description   string   `json:"description"`
+	Notes         string   `json:"notes"`
+	ResourceTypes []string `json:"resource_types"`
+	Severity      int      `json:"severity"`
+	Source        string   `json:"source"`
+	Success       []string `json:"success"`
+	Failure       []string `json:"failure"`
+	Warning       []string `json:"warning"`
+	WarnOnly      bool     `json:"warn_only"`
+}
+
+type ScanResource struct {
+	Source       string     `json:"resource_source"`
+	Address      string     `json:"resource_address"`
+	Name         string     `json:"resource_name"`
+	Type         string     `json:"resource_type"`
+	Region       string     `json:"resource_region"`
+	PassedRules  []ScanRule `json:"passed_rules"`
+	WarningRules []ScanRule `json:"warning_rules"`
+	FailedRules  []ScanRule `json:"failed_rules"`
+}
+
+type ScanRule struct {
+	Name        string `json:"rule_name"`
+	Description string `json:"rule_description"`
+	Detail      string `json:"rule_detail"`
+}
