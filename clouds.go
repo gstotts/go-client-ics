a99cb30d5b9021253d0a04e4067c6afcc63993a1
Gregory Stotts <mr.stotts@gmail.com>
2023-07-05 11:23:29 -0500

    Additional

--- clouds.go
+++ clouds.go
@@ -8,6 +8,14 @@ func (c *Client) ListCloudTypes() (CloudTypes, error) {
 	// Returns a list of all cloud types
 
 	var cloudTypes CloudTypes
-	err := c.makeRequest(http.MethodGet, "/v2/public/cloudtypes/list", nil, cloudTypes)
+	err := c.makeRequest(http.MethodGet, "/v2/public/cloudtypes/list", nil, &cloudTypes)
 	return cloudTypes, err
+}
+
+func (c *Client) ListClouds() ([]Cloud, error) {
+	// List availble configured clouds
+
+	var list Clouds
+	err := c.makeRequest(http.MethodGet, "/v2/public/clouds/list", nil, &list)
+	return list.Clouds, err
 }
--- clouds_test.go
+++ clouds_test.go
@@ -1,8 +1,17 @@
 package insightcloudsecClient
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 )
 
 func TestClouds_ListCloudTypes(t *testing.T) {
+	setup()
+	mux.HandleFunc("/v2/public/cloudtypes/list", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, getJSONFile("clouds/list_cloudtypes.json"))
+	})
+	resp, err := client.ListCloudTypes()
+	teardown()
 }
