0618e220f79973bd6960cd9849b64a45b33d87b5
Gregory Stotts <mr.stotts@gmail.com>
2023-07-05 10:56:44 -0500

    CloudTypes

--- clouds.go
+++ clouds.go
@@ -1,15 +1,13 @@
 package insightcloudsecClient
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func (c *Client) ListCloudTypes() ([]CloudType, error) {
+func (c *Client) ListCloudTypes() (CloudTypes, error) {
 	// Returns a list of all cloud types
 
 	var cloudTypes CloudTypes
 	err := c.makeRequest(http.MethodGet, "/v2/public/cloudtypes/list", nil, cloudTypes)
-	fmt.Println(cloudTypes)
-	return cloudTypes.CloudTypes, err
+	return cloudTypes, err
 }
--- models_clouds.go
+++ models_clouds.go
@@ -1,11 +1,11 @@
 package insightcloudsecClient
 
+type CloudTypes struct {
+	Clouds []CloudType `json:"clouds"`
+}
+
 type CloudType struct {
 	ID     string `json:"cloud_type_id"`
 	Access string `json:"cloud_access"`
 	Name   string `json:"name"`
-}
-
-type CloudTypes struct {
-	CloudTypes []CloudType `json:"clouds"`
 }
